d682228013a997fbb7659f99bb2c663abd46b509
eunkr82 <eun.kr82@gmail.com>
2026-01-30 05:55:30 +0900

    feat: 라우트 상수 추가

---
+++ src/shared/constants/path.ts
@@ -0,0 +1,16 @@
+export const ROUTE_PATH = {
+  HOME: '/',
+  LOGIN: '/login',
+  SIGN_UP: '/signup',
+
+  POSTS: '/posts',
+  POST_DETAIL: '/posts/:postId',
+
+  MY_PROFILE: '/me/profile',
+  MY_PORTFOLIO: '/me/portfolio',
+
+  MEMBERS: '/members',
+  MEMBERS_RECOMMEND: '/members/recommend',
+  MEMBER_PROFILE: 'members/:memberId/profile',
+  MEMBER_PORTFOLIO: 'members/:memberId/portfolio',
+} as const;
