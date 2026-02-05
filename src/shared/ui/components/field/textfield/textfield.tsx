390b0ae9977df58d5bfadb438e2f508049631d7a
eunkr82 <eun.kr82@gmail.com>
2026-02-06 08:26:42 +0900

    feat: ref 전달 추가

--- src/shared/ui/components/field/textfield/textfield.tsx
+++ src/shared/ui/components/field/textfield/textfield.tsx
@@ -1,16 +1,18 @@
 import type { ComponentPropsWithoutRef } from 'react';
+import { forwardRef } from 'react';
 
 import FieldBase from '../field-base';
 import * as styles from '../field-base.css';
 
-const TextField = (
-  props: Omit<ComponentPropsWithoutRef<'input'>, 'children'>,
-) => {
+const TextField = forwardRef<
+  HTMLInputElement,
+  Omit<ComponentPropsWithoutRef<'input'>, 'children'>
+>((props, ref) => {
   return (
     <FieldBase>
-      <input className={styles.control} {...props} />
+      <input ref={ref} className={styles.control} {...props} />
     </FieldBase>
   );
-};
+});
 
 export default TextField;
