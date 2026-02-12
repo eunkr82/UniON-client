e685eb3f1ae0f5396d8afc393b88a0e6580ed92f
eunkr82 <eun.kr82@gmail.com>
2026-02-13 02:41:29 +0900

    feat: onRemove 추가

--- src/shared/ui/components/chip/chip.tsx
+++ src/shared/ui/components/chip/chip.tsx
@@ -1,13 +1,24 @@
+import { XIcon } from '@shared/assets';
 import type { ReactNode } from 'react';
 
 import * as styles from './chip.css';
 
 interface ChipProps {
   children: ReactNode;
+  onRemove?: () => void;
 }
 
-const Chip = ({ children }: ChipProps) => {
-  return <span className={styles.chip}>{children}</span>;
+const Chip = ({ children, onRemove }: ChipProps) => {
+  return (
+    <span className={styles.chip}>
+      {children}
+      {onRemove && (
+        <button type='button' onClick={onRemove} aria-label='삭제하기'>
+          <XIcon />
+        </button>
+      )}
+    </span>
+  );
 };
 
 export default Chip;
