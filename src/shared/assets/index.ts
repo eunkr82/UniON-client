c62bc8ecaa75db4df88aba41823446bcd7735b0d
eunkr82 <eun.kr82@gmail.com>
2026-02-13 14:34:32 +0900

    chore: 파일명 컨벤션 통일

--- src/shared/assets/index.ts
+++ src/shared/assets/index.ts
@@ -1,3 +1,3 @@
-export { default as StepperLeftIcon } from './icons/icn_stepperLeft.svg?react';
-export { default as StepperRightIcon } from './icons/icn_stepperRight.svg?react';
+export { default as StepperLeftIcon } from './icons/icn-stepperLeft.svg?react';
+export { default as StepperRightIcon } from './icons/icn-stepperRight.svg?react';
 export { default as XIcon } from './icons/icn-X.svg?react';
