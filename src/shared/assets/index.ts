e108520903abede56b44dc951be67db977b77b53
eunkr82 <eun.kr82@gmail.com>
2026-02-25 05:18:44 +0900

    feat: 아이콘 추가

--- src/shared/assets/index.ts
+++ src/shared/assets/index.ts
@@ -5,4 +5,5 @@ export { default as StepperLeftIcon } from './icons/icn-stepperLeft.svg?react';
 export { default as StepperRightIcon } from './icons/icn-stepperRight.svg?react';
 export { default as TeamIcon } from './icons/icn-team.svg?react';
 export { default as ThumbnailIcon } from './icons/icn-thumbnail.svg?react';
+export { default as UploadIcon } from './icons/icn-upload.svg?react';
 export { default as XIcon } from './icons/icn-X.svg?react';
